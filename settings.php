03396ba7bfb43f43a95467e83373f33c01884a14
novicaa <novicamiketic@gmail.com>
2015-04-20 13:44:42 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,17 @@
+<?php
+$adresa[0]="407 Fifth Avenue South Naples, FL 34102"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Naples";
+$telephone[0]=2396434202;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="Closed May 2013,Read more about store closing,173 Main Street,Yarmouthport, MA, 02675"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Yarmouthport";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Cape Cod";
+$telephone[1]=2396434202;// Broj koji se poziva na dugme CALL US
+
+$companynumber=2;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
